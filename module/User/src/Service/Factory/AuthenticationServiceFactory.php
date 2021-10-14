0157dd765b78882279e7378f0a57fb47aedd387d
FEW172 <few_172@mail.ru>
2021-10-14 10:49:19 +0300

    Update

--- module/User/src/Service/Factory/AuthenticationServiceFactory.php
+++ module/User/src/Service/Factory/AuthenticationServiceFactory.php
@@ -20,7 +20,7 @@ class AuthenticationServiceFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $sessionManager = $container->get(SessionManager::class);
-        $authStorage = new SessionStorage('UserContainer', 'session', $sessionManager);
+        $authStorage = new SessionStorage('UserSession', 'session', $sessionManager);
         $authAdapter = $container->get(AuthAdapter::class);
 
         // Create the service and inject dependencies into its constructor.
